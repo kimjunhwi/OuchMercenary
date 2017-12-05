a66166cb211b8d82261f14a38655d9ad6a7070f6
Smaet <dkan56@naver.com>
2017-12-05 18:39:41 +0900

    전투 준비 캐릭터 탭 슬라이드 구현중

---
+++ Assets/Scripts/UI/PreBattle/CharacterSlot.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Events;
+using UnityEngine.EventSystems;
+
+public class CharacterSlot : MonoBehaviour, IEventSystemHandler, IPointerDownHandler, IPointerUpHandler
+{
+	public Image characterBox_Image;
+	public Image characterTribe_Image;
+	public Text characterName_Text;
+	public Text characterHealth_Text;
+	public Text characterLevel_Text;
+
+	public Button characterSpecific_Button;
+	public Slider hp_Slider;
+	public Slider levelExp_Slider;
+
+	public GameObject skillPanel_Obj;
+	public Image[] skillImage;
+	public Image[] dispatch_ImageButton;
+
+	public void OnPointerDown(PointerEventData eventData)
+	{
+
+	}
+
+	public void OnPointerUp(PointerEventData eventData)
+	{
+		Debug.Log ("OnPointerUp");
+		if (eventData.pointerCurrentRaycast.gameObject.name == "CharacterDispatch_ImageButton") 
+		{
+			Debug.Log ("CharacterDispatch_ImageButton Touch!!");
+		} 
+		else if (eventData.pointerCurrentRaycast.gameObject.name == "CharacterCancel_ImageButton ") {
+			Debug.Log ("CharacterCancel_ImageButton  Touch!!");
+		} 
+		else if (eventData.pointerCurrentRaycast.gameObject.name == "CharacterDispatching_ImageButton") {
+			Debug.Log ("CharacterDispatching_ImageButton Touch!!");
+		} 
+		else {
+			Debug.Log ("Not Click");
+		}
+	}
+}
--- Assets/Scripts/UI/PreBattle/PreBattleSceneManager.cs
+++ Assets/Scripts/UI/PreBattle/PreBattleSceneManager.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
+using ReadOnlys;
+
 public class PreBattleSceneManager : MonoBehaviour {
 
 	private const string sInfoText = "전투준비";
@@ -10,10 +12,19 @@ public class PreBattleSceneManager : MonoBehaviour {
 
 	public Button StartBattle_Button;
 
+	public GameObject [] characterTapList_Objs;
+
+
+
 	void Start()
 	{
 		//GameManager.Instance.SetUpbar (ReadOnlys.E_SCENE_INDEX.E_STAGE_PREBATTLE, canvas, sInfoText);
 		//StartBattle_Button.onClick.AddListener(() => GameManager.Instance.LoadScene (ReadOnlys.E_SCENE_INDEX.E_BATTLE, ReadOnlys.E_SCENE_INDEX.E_STAGE_PREBATTLE, canvas));
 	}
 
+	void SetCharacterSlot(E_PREPAREBATTLE_CHARCTERTYPE _index)
+	{
+		//처음에는 무조건 전체 탭에 있는 캐릭터들
+
+	}
 }
