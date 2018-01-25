6d11c1bf85bbc8dfd9da1270db127907d9d4e2f9
kimjunhwi <gustlraks@naver.com>
2018-01-25 17:22:12 +0900

    수정

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -38,250 +38,11 @@ public class Player : MonoBehaviour
 
 	int nLastStageIndex;
 
+	public int nDefenceChapterOne = 0;
+	public int nDefenceChapterTwo = 0;
 
 	public void Init()
 	{
-
-        //임시 스킬을 부여함 --------------------------------------------------- 나중에 스킬이 확정 됐을 경우 싸악 수정(Unicode파싱 해서 처리)
-        List<ActiveSkill> Warrior_Acitve_List = new List<ActiveSkill>();
-		List<ActiveSkill> Archer_Acitve_List = new List<ActiveSkill>();
-		List<ActiveSkill> Wizard_Active_List = new List<ActiveSkill> ();
-
-		ActiveSkill tempActiveSkill = null;
-
-		CharacterStats _charic = new CharacterStats();
-
-		_charic.m_nIndex = 0;
-		_charic.m_strJobName = "basicWarrior";
-		_charic.m_strJob = "Warrior";
-		_charic.m_nLevel = 1;
-		_charic.m_nTier = 1;
-		_charic.m_nAttribute = (int)E_ATTRIBUTE.E_PHYSICAL;
-		_charic.m_nAttackType = (int)E_ATTACK_TYPE.E_MELEE;
-		_charic.m_nTribe = (int)E_TRIBE.E_HUMAN;
-		_charic.m_fSite = 5;
-		_charic.m_fHealth = 50;
-		_charic.m_fAccuracy = 5;
-		_charic.m_fPhyiscal_Rating = 5;
-		_charic.m_fMagic_Rating = 5;
-		_charic.m_fAttack_Range = 1;
-		_charic.m_fAttackSpeed = 1.0f;
-		_charic.m_fMoveSpeed = 1;
-		_charic.m_fPhysical_Defence = 5;
-		_charic.m_fMasic_Defence = 5;
-		_charic.m_fDodge = 1;
-		_charic.m_fCritical_Rating = 1;
-		_charic.m_fCritical_Damage = 1.5f;
-		_charic.m_nBatchIndex = 12;
-
-//
-//
-//		_charic.basicSkill.Add(new BasicSkill(1,1001,"a","attack",0,1,"warrior",1,1,100,0,1.0f,"enemy",1,1,"close","p_attack rating의 100%로 공격"));
-//
-//		//Active
-//		tempActiveSkill = new ActiveSkill(0,1000,"double Attack","0,10",2,1,"Warrior",1,1,0,20,0,0,0,0,0,6,0,75,0,1,1.5f,1.5f,1,"close",0,0,"","","pAttack ratring의 75%로 2회 공격",false);
-//			
-//		Warrior_Acitve_List.Add(tempActiveSkill);
-//
-//		tempActiveSkill = new ActiveSkill(0,1001,"Power Attack","0,10",2,1,"Warrior",1,1,0,20,0,0,0,0,0,6,0,120,0,1,1.5f,1.5f,1,"close",0,0,"","","pAttack ratring의 120%로 2회 공격",false);
-//
-//		Warrior_Acitve_List.Add(tempActiveSkill);
-
-		//높은것을 정렬retur
-		Warrior_Acitve_List.Sort(delegate(ActiveSkill A, ActiveSkill B)
-		{
-			if (A.m_nTier < B.m_nTier) return 1;
-
-			else return 0;
-		});
-
-		_charic.activeSkill = Warrior_Acitve_List;
-        LIST_HERO.Add(_charic);
-		_charic = new CharacterStats ();
-
-		_charic.m_nIndex = 1;
-		_charic.m_strJobName = "basicArcher";
-		_charic.m_strJob = "Archer";
-		_charic.m_nLevel = 1;
-		_charic.m_nTier = 1;
-		_charic.m_nAttribute = (int)E_ATTRIBUTE.E_PHYSICAL;
-		_charic.m_nAttackType = (int)E_ATTACK_TYPE.E_RANGE;
-		_charic.m_nTribe = (int)E_TRIBE.E_HUMAN;
-		_charic.m_fSite = 10;
-		_charic.m_fHealth = 500;
-		_charic.m_fAccuracy = 10;
-		_charic.m_fPhyiscal_Rating = 5;
-		_charic.m_fMagic_Rating = 5;
-		_charic.m_fAttack_Range = 5;
-		_charic.m_fAttackSpeed = 1.0f;
-		_charic.m_fMoveSpeed = 1;
-		_charic.m_fPhysical_Defence = 5;
-		_charic.m_fMasic_Defence = 5;
-		_charic.m_fDodge = 1;
-		_charic.m_fCritical_Rating = 1;
-		_charic.m_fCritical_Damage = 1.5f;
-		_charic.m_nBatchIndex = 2;
-
-		_charic.basicSkill.Add(new BasicSkill(1,1001,"a","attack",0,1,"archer",1,1,100,0,1.0f,"enemy",1,1,"close", "-1", "p_attack rating의 100%로 공격"));
-
-//		//Active
-//		tempActiveSkill = new ActiveSkill(1,1001,"Power Attack","9",2,1,"archer",1,1,0,20,0,0,0,0,0,6,0,120,0,1,1.5f,1.5f,1,"close",0,0,"","","pAttack ratring의 120%로 2회 공격",false);
-//			
-//		Archer_Acitve_List.Add(tempActiveSkill);
-//
-//		_charic.activeSkill = Archer_Acitve_List;
-
-		LIST_HERO.Add (_charic);
-
-		_charic = new CharacterStats ();
-
-		_charic.m_nIndex = 2;
-		_charic.m_strJobName = "basicPriest";
-		_charic.m_strJob = "Priest";
-		_charic.m_nLevel = 1;
-		_charic.m_nTier = 1;
-		_charic.m_nAttribute = (int)E_ATTRIBUTE.E_PHYSICAL;
-		_charic.m_nAttackType = (int)E_ATTACK_TYPE.E_RANGE;
-		_charic.m_nTribe = (int)E_TRIBE.E_HUMAN;
-		_charic.m_fSite = 10;
-		_charic.m_fHealth = 500;
-		_charic.m_fAccuracy = 10;
-		_charic.m_fPhyiscal_Rating = 5;
-		_charic.m_fMagic_Rating = 5;
-		_charic.m_fAttack_Range = 100f;
-		_charic.m_fAttackSpeed = 1.0f;
-		_charic.m_fMoveSpeed = 1;
-		_charic.m_fPhysical_Defence = 5;
-		_charic.m_fMasic_Defence = 5;
-		_charic.m_fDodge = 1;
-		_charic.m_fCritical_Rating = 1;
-		_charic.m_fCritical_Damage = 1.5f;
-		_charic.m_nBatchIndex = 3;
-
-		_charic.basicSkill.Add(new BasicSkill(5,1005,"a","attack",0,1,"priest",1,1,100,100,1.0f,"allay",1,1,"close", "-1",""));
-
-		LIST_HERO.Add (_charic);
-
-
-		_charic = new CharacterStats ();
-
-		_charic.m_nIndex = 3;
-		_charic.m_strJobName = "basicKnight";
-		_charic.m_strJob = "Knight";
-		_charic.m_nLevel = 1;
-		_charic.m_nTier = 1;
-		_charic.m_nAttribute = (int)E_ATTRIBUTE.E_PHYSICAL;
-		_charic.m_nAttackType = (int)E_ATTACK_TYPE.E_RANGE;
-		_charic.m_nTribe = (int)E_TRIBE.E_HUMAN;
-		_charic.m_fSite = 10;
-		_charic.m_fHealth = 500;
-		_charic.m_fAccuracy = 10;
-		_charic.m_fPhyiscal_Rating = 5;
-		_charic.m_fMagic_Rating = 5;
-		_charic.m_fAttack_Range = 1f;
-		_charic.m_fAttackSpeed = 1.0f;
-		_charic.m_fMoveSpeed = 1;
-		_charic.m_fPhysical_Defence = 5;
-		_charic.m_fMasic_Defence = 5;
-		_charic.m_fDodge = 1;
-		_charic.m_fCritical_Rating = 1;
-		_charic.m_fCritical_Damage = 1.5f;
-		_charic.m_nBatchIndex = 5;
-
-		_charic.basicSkill.Add(new BasicSkill(5,1005,"a","attack",0,1,"priest",1,1,100,100,1.0f,"allay",1,1,"close", "-1", ""));
-
-		LIST_HERO.Add (_charic);
-
-		_charic = new CharacterStats ();
-
-		_charic.m_nIndex = 4;
-		_charic.m_strJobName = "basicMechanic";
-		_charic.m_strJob = "Mechanic";
-		_charic.m_nLevel = 1;
-		_charic.m_nTier = 1;
-		_charic.m_nAttribute = (int)E_ATTRIBUTE.E_PHYSICAL;
-		_charic.m_nAttackType = (int)E_ATTACK_TYPE.E_RANGE;
-		_charic.m_nTribe = (int)E_TRIBE.E_HUMAN;
-		_charic.m_fSite = 10;
-		_charic.m_fHealth = 500;
-		_charic.m_fAccuracy = 10;
-		_charic.m_fPhyiscal_Rating = 5;
-		_charic.m_fMagic_Rating = 5;
-		_charic.m_fAttack_Range = 5f;
-		_charic.m_fAttackSpeed = 1.0f;
-		_charic.m_fMoveSpeed = 1;
-		_charic.m_fPhysical_Defence = 5;
-		_charic.m_fMasic_Defence = 5;
-		_charic.m_fDodge = 1;
-		_charic.m_fCritical_Rating = 1;
-		_charic.m_fCritical_Damage = 1.5f;
-		_charic.m_nBatchIndex = 6;
-
-		_charic.basicSkill.Add(new BasicSkill(1,1001,"a","attack",0,1,"archer",1,1,100,0,1.0f,"enemy",1,1,"close", "-1", "p_attack rating의 100%로 공격"));
-
-		LIST_HERO.Add (_charic);
-
-		_charic = new CharacterStats ();
-
-		_charic.m_nIndex = 5;
-		_charic.m_strJobName = "basicWizard";
-		_charic.m_strJob = "Wizard";
-		_charic.m_nLevel = 1;
-		_charic.m_nTier = 1;
-		_charic.m_nAttribute = (int)E_ATTRIBUTE.E_PHYSICAL;
-		_charic.m_nAttackType = (int)E_ATTACK_TYPE.E_RANGE;
-		_charic.m_nTribe = (int)E_TRIBE.E_HUMAN;
-		_charic.m_fSite = 10;
-		_charic.m_fHealth = 500;
-		_charic.m_fAccuracy = 10;
-		_charic.m_fPhyiscal_Rating = 5;
-		_charic.m_fMagic_Rating = 5;
-		_charic.m_fAttack_Range = 5f;
-		_charic.m_fAttackSpeed = 1.0f;
-		_charic.m_fMoveSpeed = 1;
-		_charic.m_fPhysical_Defence = 5;
-		_charic.m_fMasic_Defence = 5;
-		_charic.m_fDodge = 1;
-		_charic.m_fCritical_Rating = 1;
-		_charic.m_fCritical_Damage = 1.5f;
-		_charic.m_nBatchIndex = 9;
-
-		_charic.basicSkill.Add(new BasicSkill(1,1000,"a","attack",0,1,"wizard",1,1,100,0,1.0f,"enemy",1,1,"close", "-1", "p_attack rating의 100%로 공격"));
-
-//		tempActiveSkill = new ActiveSkill(1,1001,"Power Attack","9",2,1,"wizard",1,1,0,100,0,0,0,0,0,6,10,120,0,1,1.5f,1.5f,1,"close",0,0,"","","pAttack ratring의 120%로 2회 공격",false);
-//
-//		Wizard_Active_List.Add(tempActiveSkill);
-//
-//		_charic.activeSkill = Wizard_Active_List;
-
-		LIST_HERO.Add (_charic);
-
-		_charic = new CharacterStats ();
-
-		_charic.m_nIndex = 6;
-		_charic.m_strJobName = "basicCommander";
-		_charic.m_strJob = "Commander";
-		_charic.m_nLevel = 1;
-		_charic.m_nTier = 1;
-		_charic.m_nAttribute = (int)E_ATTRIBUTE.E_PHYSICAL;
-		_charic.m_nAttackType = (int)E_ATTACK_TYPE.E_MELEE;
-		_charic.m_nTribe = (int)E_TRIBE.E_HUMAN;
-		_charic.m_fSite = 10;
-		_charic.m_fHealth = 500;
-		_charic.m_fAccuracy = 10;
-		_charic.m_fPhyiscal_Rating = 5;
-		_charic.m_fMagic_Rating = 5;
-		_charic.m_fAttack_Range = 5;
-		_charic.m_fAttackSpeed = 1.0f;
-		_charic.m_fMoveSpeed = 1;
-		_charic.m_fPhysical_Defence = 5;
-		_charic.m_fMasic_Defence = 5;
-		_charic.m_fDodge = 1;
-		_charic.m_fCritical_Rating = 1;
-		_charic.m_fCritical_Damage = 1.5f;
-		_charic.m_nBatchIndex = 7;
-
-		LIST_HERO.Add (_charic);
 
 		for (int i = 0; i < 53; i++) {
 			LIST_CHARACTER.Add ( GameManager.Instance.lDbBasicCharacter [i]);
@@ -296,6 +57,249 @@ public class Player : MonoBehaviour
 		GameManager.Instance.CharacterBoxImageLoad (characterBoxStr06);
 		GameManager.Instance.CharacterBoxImageLoad (characterBoxStr07);
 	}
+
+	public Equipment AddItem(E_EQUIMENT_TYPE _type, int _nIndex)
+	{
+		Equipment addEquipment =new Equipment();
+
+		switch (_type) 
+		{
+		case E_EQUIMENT_TYPE.E_WEAPON:
+			{
+				DBWeapon weaponDB = GameManager.Instance.lDbWeapon [_nIndex];
+
+				addEquipment.nIndex = weaponDB.nIndex;
+				addEquipment.strName = weaponDB.sName;
+				addEquipment.nTier = weaponDB.nTier;
+				addEquipment.nQulity = weaponDB.nQulity;
+				addEquipment.strPossibleJob = weaponDB.sJob;
+				addEquipment.nEnhance = weaponDB.nEnhanced;
+				addEquipment.strEquimnetType = weaponDB.sEquipType;
+				addEquipment.fPhysical_Attack_Rating = weaponDB.fPhysical_AttackRating;
+				addEquipment.fMagic_Attack_Rating = weaponDB.fMagic_AttackRating;
+
+				addEquipment.nSellCost = weaponDB.nSellCost;
+				addEquipment.nMakeMaterialIndex = weaponDB.nMakeMaterial;
+				addEquipment.nBreakMaterialIndex = weaponDB.nBreakMaterial;
+			}
+			break;
+		case E_EQUIMENT_TYPE.E_ARMOR:
+			{
+				DBArmor weaponDB = GameManager.Instance.lDBArmor [_nIndex];
+
+				addEquipment.nIndex = weaponDB.nIndex;
+				addEquipment.strName = weaponDB.sName;
+				addEquipment.nTier = weaponDB.nTier;
+				addEquipment.nQulity = weaponDB.nQulity;
+				addEquipment.strPossibleJob = weaponDB.sJob;
+				addEquipment.nEnhance = weaponDB.nEnhanced;
+				addEquipment.strEquimnetType = weaponDB.sEquipType;
+				addEquipment.fPhysical_Defense = weaponDB.fPhysical_Defense;
+				addEquipment.fMagic_Defense = weaponDB.fMagic_Defense;
+
+				addEquipment.nSellCost = weaponDB.nSellCost;
+				addEquipment.nMakeMaterialIndex = weaponDB.nMakeMaterial;
+				addEquipment.nBreakMaterialIndex = weaponDB.nBreakMaterial;
+			}
+			break;
+		case E_EQUIMENT_TYPE.E_GLOVE:
+			{
+				DBGlove weaponDB = GameManager.Instance.lDBGlove [_nIndex];
+
+				addEquipment.nIndex = weaponDB.nIndex;
+				addEquipment.strName = weaponDB.sName;
+				addEquipment.nTier = weaponDB.nTier;
+				addEquipment.nQulity = weaponDB.nQulity;
+				addEquipment.strPossibleJob = weaponDB.sJob;
+				addEquipment.nEnhance = weaponDB.nEnhanced;
+				addEquipment.strEquimnetType = weaponDB.sEquipType;
+				addEquipment.fPhysical_Defense = weaponDB.fPhysical_Defense;
+				addEquipment.fMagic_Defense = weaponDB.fMagic_Defense;
+
+				addEquipment.nSellCost = weaponDB.nSellCost;
+				addEquipment.nMakeMaterialIndex = weaponDB.nMakeMaterial;
+				addEquipment.nBreakMaterialIndex = weaponDB.nBreakMaterial;
+			}
+			break;
+		case E_EQUIMENT_TYPE.E_ACCESSORY:
+			{
+				DBAccessory weaponDB = GameManager.Instance.lDBAccessory [_nIndex];
+
+				addEquipment.nIndex = weaponDB.nIndex;
+				addEquipment.strName = weaponDB.sName;
+				addEquipment.nTier = weaponDB.nTier;
+				addEquipment.nQulity = weaponDB.nQulity;
+				addEquipment.strPossibleJob = weaponDB.sJob;
+				addEquipment.nEnhance = weaponDB.nEnhanced;
+				addEquipment.strEquimnetType = weaponDB.sEquipType;
+
+				addEquipment.nSellCost = weaponDB.nSellCost;
+				addEquipment.nMakeMaterialIndex = weaponDB.nMakeMaterial;
+				addEquipment.nBreakMaterialIndex = weaponDB.nBreakMaterial;
+			}
+			break;
+		}
+
+		RandomAddOption (addEquipment, addEquipment.strEquimnetType);
+
+		return addEquipment;
+	}
+
+	public void RandomAddOption(Equipment _ITEM,string _strRandomOption)
+	{
+		string[] strRandomOptions = _strRandomOption.Split (',');
+
+		int nRandomIndex = int.Parse(strRandomOptions [Random.Range (0, strRandomOptions.Length)]);
+
+		DBEquipment_RandomOption randomDB = GameManager.Instance.lDBEquipmentRandomOption [nRandomIndex];
+
+		int nIndex = 0;
+
+		while (nIndex < _ITEM.nQulity) 
+		{
+			switch (randomDB.nOptionIndex) {
+			case (int)E_RANDOM_OPTION.E_HP:
+				{
+					if (_ITEM.fHp != 0)
+						continue;
+
+					_ITEM.fHp = (int)Random.Range (randomDB.nStartValue, randomDB.nEndValue);
+				}
+				break;
+			case (int)E_RANDOM_OPTION.E_ACCURACY:
+				{
+					if (_ITEM.fAccuracy != 0)
+						continue;
+
+					_ITEM.fAccuracy = (int)Random.Range (randomDB.nStartValue, randomDB.nEndValue);
+				}
+				break;
+			case (int)E_RANDOM_OPTION.E_ALL_ATTACK_RATING:
+				{
+					if (_ITEM.fAll_Attack_RatingPlus != 0)
+						continue;
+
+					_ITEM.fAll_Attack_RatingPlus = (int)Random.Range (randomDB.nStartValue, randomDB.nEndValue);
+				}
+				break;
+			case (int)E_RANDOM_OPTION.E_PHSYICAL_ATTACK_RATING:
+				{
+					if (_ITEM.fPhysical_Attack_Rating != 0)
+						continue;
+
+					_ITEM.fPhysical_Attack_Rating = (int)Random.Range (randomDB.nStartValue, randomDB.nEndValue);
+				}
+				break;
+			case (int)E_RANDOM_OPTION.E_MAGIC_ATTACK_RATING:
+				{
+					if (_ITEM.fMagic_Attack_Rating != 0)
+						continue;
+
+					_ITEM.fMagic_Attack_Rating = (int)Random.Range (randomDB.nStartValue, randomDB.nEndValue);
+				}
+				break;
+			case (int)E_RANDOM_OPTION.E_ALL_PENETRATE:
+				{
+					if (_ITEM.fAll_Penetrate != 0)
+						continue;
+
+					_ITEM.fAll_Penetrate = (int)Random.Range (randomDB.nStartValue, randomDB.nEndValue);
+				}
+				break;
+			case (int)E_RANDOM_OPTION.E_PHSYICAL_PENETRATE:
+				{
+					if (_ITEM.fPhysical_Penetrate != 0)
+						continue;
+
+					_ITEM.fPhysical_Penetrate = (int)Random.Range (randomDB.nStartValue, randomDB.nEndValue);
+				}
+				break;
+			case (int)E_RANDOM_OPTION.E_MAGIC_PENETRATE:
+				{
+					if (_ITEM.fMagic_Penetrate != 0)
+						continue;
+
+					_ITEM.fMagic_Penetrate = (int)Random.Range (randomDB.nStartValue, randomDB.nEndValue);
+				}
+				break;
+			case (int)E_RANDOM_OPTION.E_ALL_DEFENSE:
+				{
+					if (_ITEM.fAll_Defense != 0)
+						continue;
+
+					_ITEM.fAll_Defense = (int)Random.Range (randomDB.nStartValue, randomDB.nEndValue);
+				}
+				break;
+			case (int)E_RANDOM_OPTION.E_PHSYICAL_DEFENSE:
+				{
+					if (_ITEM.fPhysical_Defense != 0)
+						continue;
+
+					_ITEM.fPhysical_Defense = (int)Random.Range (randomDB.nStartValue, randomDB.nEndValue);
+				}
+				break;
+			case (int)E_RANDOM_OPTION.E_MAGIC_DEFENSE:
+				{
+					if (_ITEM.fMagic_Defense != 0)
+						continue;
+
+					_ITEM.fMagic_Defense = (int)Random.Range (randomDB.nStartValue, randomDB.nEndValue);
+				}
+				break;
+			case (int)E_RANDOM_OPTION.E_DODGE:
+				{
+					if (_ITEM.fDodge != 0)
+						continue;
+
+					_ITEM.fDodge = (int)Random.Range (randomDB.nStartValue, randomDB.nEndValue);
+				}
+				break;
+			case (int)E_RANDOM_OPTION.E_CRITICAL_RATING:
+				{
+					if (_ITEM.fCritical_Rating != 0)
+						continue;
+
+					_ITEM.fCritical_Rating = (int)Random.Range (randomDB.nStartValue, randomDB.nEndValue);
+				}
+				break;
+			case (int)E_RANDOM_OPTION.E_CRITICAL_DAMAGE:
+				{
+					if (_ITEM.fCritical_Damage != 0)
+						continue;
+
+					_ITEM.fCritical_Damage = (int)Random.Range (randomDB.nStartValue, randomDB.nEndValue);
+				}
+				break;
+			case (int)E_RANDOM_OPTION.E_ATTACK_SPEED:
+				{
+					if (_ITEM.fAttack_Speed != 0)
+						continue;
+
+					_ITEM.fAttack_Speed = (int)Random.Range (randomDB.nStartValue, randomDB.nEndValue);
+				}
+				break;
+			case (int)E_RANDOM_OPTION.E_COOLTIME:
+				{
+					if (_ITEM.fCoolTime != 0)
+						continue;
+
+					_ITEM.fCoolTime = (int)Random.Range (randomDB.nStartValue, randomDB.nEndValue);
+				}
+				break;
+			case (int)E_RANDOM_OPTION.E_EXP_BOOST:
+				{
+					if (_ITEM.fExpBoost != 0)
+						continue;
+
+					_ITEM.fExpBoost = (int)Random.Range (randomDB.nStartValue, randomDB.nEndValue);
+				}
+				break;
+			}
+
+			nIndex++;
+		}
+	}
+
 
 	public void AddCharacter(int _nIndex)
 	{
